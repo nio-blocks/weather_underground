79705daae9f05b681fc44a3c3b46be8eccc7f5d1
Chris Brackert <cbrackert@gmail.com>
2015-04-23 15:19:56 -0700

    Add WeatherUndergroundForecast10Day block

---
+++ tests/test_weather_underground_forecast10day_block.py
@@ -0,0 +1,59 @@
+from urllib.request import quote
+from unittest.mock import patch
+from requests import Response
+from ..weather_underground_forecast10day_block \
+    import WeatherUndergroundForecast10Day
+from nio.util.support.block_test_case import NIOBlockTestCase
+from nio.modules.threading import Event
+
+
+class WUnderTest(WeatherUndergroundForecast10Day):
+    def __init__(self, event):
+        super().__init__()
+        self._event = event
+
+    def _process_response(self, resp):
+        signals, paging = super()._process_response(resp)
+        self._event.set()
+        return signals, paging
+
+
+class TestWeatherUndergroundForecast10Day(NIOBlockTestCase):
+
+    @patch("requests.get")
+    @patch("requests.Response.json")
+    def test_process_response(self, mock_json, mock_get):
+        mock_get.return_value = Response()
+        mock_get.return_value.status_code = 200
+        mock_json.return_value = {
+            'forecast': {
+                'stuff': 'things'
+            }
+        }
+        e = Event()
+        blk = WUnderTest(e)
+        city = 'San Francisco'
+        state = 'California'
+        self.configure_block(blk, {
+            'polling_interval': {
+                'seconds': 1
+            },
+            'retry_interval': {
+                'seconds': 1
+            },
+            'queries': [{
+                'state': state,
+                'city': city
+            }],
+        })
+        blk.start()
+        e.wait(2)
+        self.assert_num_signals_notified(1)
+        self.assertEqual(mock_get.call_args[0][0],
+                         blk.URL_FORMAT.format(
+                             blk.api_key,
+                             'forecast10day',
+                             quote(state),
+                             quote(city))
+                         )
+        blk.stop()
--- weather_underground_forecast10day_block.py
+++ weather_underground_forecast10day_block.py
@@ -3,11 +3,11 @@ from .weather_underground_base import WeatherUndergroundBase
 
 
 @Discoverable(DiscoverableType.block)
-class WeatherUnderground(WeatherUndergroundBase):
+class WeatherUndergroundForecast10Day(WeatherUndergroundBase):
     """ This block polls the Weather Underground API, grabbing the
-    weather conditions in a given location.
+    weather forecast10day in a given location.
 
-    http://www.wunderground.com/weather/api/d/docs?d=data/conditions
+    http://www.wunderground.com/weather/api/d/docs?d=data/forecast10day&MR=1
 
     Params:
         queries (list): List of city/states to poll for weeather conds.
@@ -16,5 +16,5 @@ class WeatherUnderground(WeatherUndergroundBase):
 
     def __init__(self):
         super().__init__()
-        self._api_endpoint = 'conditions'
-        self._response_key = 'current_observation'
+        self._api_endpoint = 'forecast10day'
+        self._response_key = 'forecast'
